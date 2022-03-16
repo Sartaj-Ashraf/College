09c7e69f877e0b82cd701a50582dd494300e221c
Sartaj-Ashraf <sartajashraf842@gmail.com>
2022-03-16 17:01:28 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+        var typed = new Typed('#typing', {
+            strings: [
+            "EDUCATION WITH TECHNOLOGIES",
+            "Education with technologies",
+            "PLACE TO ACHEIVE GOOD RESULTS",
+            "TURNING GOALS INTO REALITY",
+            "PREPARE FOR SUCCESSFUL FUTURE"],
+            typeSpeed: 40,
+            startDelay: 600,
+            backSpeed: 20,
+            backDelay: 1500,
+            loop: true,
+            loopCount:10,
+            showCursor: false,
+          });
+
